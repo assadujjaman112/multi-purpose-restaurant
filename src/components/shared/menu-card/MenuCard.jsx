7dcfea632036d2ec42adfb30158ad44a48f31614
assadujjaman112 <assadujjamanmurad120@gmail.com>
2026-04-27 22:01:37 +0600

    fixed the menu card bug

--- src/components/shared/menu-card/MenuCard.jsx
+++ src/components/shared/menu-card/MenuCard.jsx
@@ -30,13 +30,13 @@ const MenuCard = ({ item }) => {
     <div className="relative">
       <Link to={`/menu/${item._id}`}>
         <div className="flex flex-col lg:flex-row gap-7 p-4 md:p-0 w-full h-full">
-          {/* <div className="w-[170px] h-[170px]"> */}
-          <img
-            src={item?.image}
-            alt=""
-            className="w-full h-48 lg:w-[180px] lg:h-full object-cover"
-          />
-          {/* </div> */}
+          <div className="w-full h-48 lg:w-[160px] lg:h-auto shrink-0">
+            <img
+              src={item?.image}
+              alt=""
+              className="w-full h-full object-cover"
+            />
+          </div>
           <div className="py-2">
             <h4 className="text-white font-elsie font-medium text-3xl hover:text-[#FFDE9F]">
               {item?.name}
