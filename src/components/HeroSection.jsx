64aac13bbed361ce84fc50dede80b8364c749cb3
assadujjaman112 <assadujjamanmurad120@gmail.com>
2026-05-02 02:40:21 +0600

    fixed bug in the hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -24,14 +24,11 @@ const HeroSection = () => {
           </div>
         </div>
         <div className="mt-10 flex justify-center md:w-[60%] lg:w-[50%] md:justify-end lg:justify-center">
-          <Link to="/menu">
-            <button
-              className="bg-[#FFDE9F] 
-           px-5 py-2 lg:px-10 lg:py-5 text-lg lg:text-xl  font-medium hover:bg-black border-[#FFDE9F] border hover:text-[#FFDE9F]
-          "
-            >
-              Discover Menu
-            </button>
+          <Link
+            to="/menu"
+            className="bg-[#FFDE9F] px-5 py-2 lg:px-10 lg:py-5 text-lg lg:text-xl font-medium hover:bg-black border-[#FFDE9F] border hover:text-[#FFDE9F]"
+          >
+            Discover Menu
           </Link>
         </div>
       </div>
