15f325e5cae9908cb825059d8ae8512cc3843eb9
assadujjaman112 <assadujjamanmurad120@gmail.com>
2026-05-03 02:28:47 +0600

    fixed bug in overview section

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -12,36 +12,36 @@ const Overview = ({ background }) => {
           <div className="flex justify-center mb-7">
             <IoMdHappy className="text-[#FFDE9F] text-9xl font-bold text-center" />
           </div>
-          <h1 className="text-[#FFDE9F] text-6xl font-semibold italic mb-8">
+          <p className="text-[#FFDE9F] text-6xl font-semibold italic mb-8">
             7.5K
-          </h1>
+          </p>
           <p className="font-lexend text-white text-2xl">Happy Customer</p>
         </div>
         <div className="border-[3px] hover:-translate-y-5 hover:shadow-lg hover:shadow-[#FFDE9F] duration-700  border-white flex-col justify-center items-center text-center py-12 px-7 bg-[#0b1315]">
           <div className="flex justify-center mb-7">
             <PiChefHatDuotone className="text-[#FFDE9F] text-9xl font-bold text-center" />
           </div>
-          <h1 className="text-[#FFDE9F] text-6xl font-semibold italic mb-8">
+          <p className="text-[#FFDE9F] text-6xl font-semibold italic mb-8">
             36
-          </h1>
+          </p>
           <p className="font-lexend text-white text-2xl">Passionate Chef’s</p>
         </div>
         <div className="border-[3px] hover:-translate-y-5 hover:shadow-lg hover:shadow-[#FFDE9F] duration-700  border-white flex-col justify-center items-center text-center py-12 px-7 bg-[#0b1315]">
           <div className="flex justify-center mb-7">
             <BiSolidDish className="text-[#FFDE9F] text-9xl font-bold text-center" />
           </div>
-          <h1 className="text-[#FFDE9F] text-6xl font-semibold italic mb-8">
+          <p className="text-[#FFDE9F] text-6xl font-semibold italic mb-8">
             250+
-          </h1>
+          </p>
           <p className="font-lexend text-white text-2xl">Favourite Dishes</p>
         </div>
         <div className="border-[3px] hover:-translate-y-5 hover:shadow-md hover:shadow-[#FFDE9F] duration-700  border-white flex-col justify-center items-center text-center py-12 px-7 bg-[#0b1315]">
           <div className="flex justify-center mb-7">
             <RxStarFilled className="text-[#FFDE9F] text-9xl font-bold text-center" />
           </div>
-          <h1 className="text-[#FFDE9F] text-6xl font-semibold italic mb-8">
+          <p className="text-[#FFDE9F] text-6xl font-semibold italic mb-8">
             4.8
-          </h1>
+          </p>
           <p className="font-lexend text-white text-2xl">Customer Rating</p>
         </div>
       </div>
