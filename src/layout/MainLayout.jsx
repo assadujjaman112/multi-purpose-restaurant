c8017dd9370e9a20607d08856efecf77b6a9b790
assadujjaman112 <assadujjamanmurad120@gmail.com>
2026-05-05 16:26:56 +0600

    updated aos refresh for newly added elements

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,13 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/shared/footer/Footer";
 import Navbar from "../components/shared/navbar/Navbar";
 
-
 const MainLayout = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        if (window.AOS) {
+            window.AOS.refresh();
+        }
+    }, [pathname]);
+
     return (
         <div>
             <Navbar />
-            <Outlet></Outlet>
+            <Outlet />
             <Footer />
         </div>
     );
