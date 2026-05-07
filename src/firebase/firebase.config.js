d2d9908832e3a9ddb43e0de99ab268185d16d40c
assadujjaman112 <assadujjamanmurad120@gmail.com>
2026-05-07 23:39:45 +0600

    moved firebase config to env file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,16 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAiD4JBj70lAbVdNHfdlQYvW_jqQy6IZIU",
-  authDomain: "multi-perpuse-restaurant.firebaseapp.com",
-  projectId: "multi-perpuse-restaurant",
-  storageBucket: "multi-perpuse-restaurant.firebasestorage.app",
-  messagingSenderId: "323405869422",
-  appId: "1:323405869422:web:f2fcdd463684b5cef2917f",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
