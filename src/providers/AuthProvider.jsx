d506959c2d9cf6c01319c075d0d93ee51ecfcf7c
assadujjaman112 <assadujjamanmurad120@gmail.com>
2026-05-11 23:44:09 +0600

    refactor AuthProvider to remove unnecessary loading state calls in user authentication methods

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -20,22 +20,18 @@ const AuthProvider = ({ children }) => {
   const googleProvider = new GoogleAuthProvider();
 
   const createUser = (email, password) => {
-    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   const signIn = (email, password) => {
-    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
   const googleSignIn = () => {
-    setLoading(true);
     return signInWithPopup(auth, googleProvider);
   };
 
   const logOut = () => {
-    setLoading(true);
     return signOut(auth);
   };
 
