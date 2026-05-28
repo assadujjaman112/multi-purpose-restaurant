4d5d97037537cec93ac851fbd81e3f8c724440fa
assadujjaman112 <assadujjamanmurad120@gmail.com>
2026-05-28 23:15:22 +0600

    refactor SignUp component to enhance accessibility by replacing spans with labels and adding required attributes to input fields

--- src/pages/sign-up/SignUp.jsx
+++ src/pages/sign-up/SignUp.jsx
@@ -92,50 +92,57 @@ const SignUp = () => {
           </h4>
           <form onSubmit={handleSignUp} className="w-full">
             <div className="flex flex-col w-full mb-2">
-              <span className="font-elsie text-[#FFDE9F] pl-2 mb-1">Name</span>
+              <label htmlFor="signup-name" className="font-elsie text-[#FFDE9F] pl-2 mb-1">Name</label>
               <input
+                id="signup-name"
                 type="text"
                 name="name"
                 placeholder="Enter Your Name"
+                autoComplete="name"
+                required
                 className="w-full outline-none py-2 pl-3 placeholder:text-[#D3D3D3] bg-transparent border-2 border-[#FFDE9F] text-white"
               />
             </div>
             <div className="flex flex-col w-full mb-2">
-              <span className="font-elsie text-[#FFDE9F] pl-2 mb-1">Email</span>
+              <label htmlFor="signup-email" className="font-elsie text-[#FFDE9F] pl-2 mb-1">Email</label>
               <input
+                id="signup-email"
                 type="email"
                 name="email"
                 placeholder="Enter Your Email"
+                autoComplete="email"
+                required
                 className="w-full outline-none py-2 pl-3 placeholder:text-[#D3D3D3] bg-transparent border-2 border-[#FFDE9F] text-white"
               />
             </div>
             <div className="flex flex-col w-full mb-2">
-              <span className="font-elsie text-[#FFDE9F] pl-2 mb-1">
-                PhotoURL
-              </span>
+              <label htmlFor="signup-image" className="font-elsie text-[#FFDE9F] pl-2 mb-1">Photo URL</label>
               <input
-                type="text"
+                id="signup-image"
+                type="url"
                 name="image"
                 placeholder="Enter Your Photo URL"
-                className="w-full outline-none py-2 pl-3  placeholder:text-[#D3D3D3] bg-transparent border-2 border-[#FFDE9F] text-white"
+                className="w-full outline-none py-2 pl-3 placeholder:text-[#D3D3D3] bg-transparent border-2 border-[#FFDE9F] text-white"
               />
             </div>
             <div className="flex flex-col w-full mb-2">
-              <span className="font-elsie text-[#FFDE9F] pl-2 mb-1">
-                Password
-              </span>
+              <label htmlFor="signup-password" className="font-elsie text-[#FFDE9F] pl-2 mb-1">Password</label>
               <input
+                id="signup-password"
                 type="password"
                 name="password"
                 placeholder="Enter Your Password"
-                className="w-full outline-none py-2 pl-3  placeholder:text-[#D3D3D3] bg-transparent border-2 border-[#FFDE9F] text-white"
+                autoComplete="new-password"
+                required
+                className="w-full outline-none py-2 pl-3 placeholder:text-[#D3D3D3] bg-transparent border-2 border-[#FFDE9F] text-white"
               />
             </div>
-            <input
+            <button
               type="submit"
-              value="Sign Up"
-              className="bg-[#FFDE9F] hover:cursor-pointer mt-5 w-full py-2 text-lg lg:text-xl  font-medium hover:bg-zinc-800 border-[#FFDE9F] border hover:text-[#FFDE9F]"
-            />
+              className="bg-[#FFDE9F] hover:cursor-pointer mt-5 w-full py-2 text-lg lg:text-xl font-medium hover:bg-zinc-800 border-[#FFDE9F] border hover:text-[#FFDE9F]"
+            >
+              Sign Up
+            </button>
           </form>
           <button
             type="button"
